cb658db1e76e794949366014168b64618ab0077e
Duong <duong@Duongs-Mac-mini.local>
2022-10-05 11:57:38 +0700

    first init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
+import { Column } from './Column';
 import logo from './logo.svg';
-import './App.css';
+import {
+  AppContainer,
+  CardContainer,
+  ColumnContainer,
+  ColumnTitle,
+} from './styles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Column text='Todo:' />
+    </AppContainer>
   );
 }
 
